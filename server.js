4f0b4df8342a8aadb9ad1b0a551f4f7670cb1b48
Sara <saralkarsh@gmail.com>
2019-11-21 13:15:24 -0700

    Pseudocode GET endpoints

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const fs = require('fs')
 const csv = require('csv-parser')
+const express = require('express')
+const app = express()
 const results = []
 
 fs.createReadStream('./csv/onehitwonders.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    console.log(results[0])
+    const example = results.slice(0, 10)
+    console.log(example)
   })
+
+app.set('port', process.env.PORT || 3000)
+app.use(express.json())
+app.locals.title = "Sports' One Hit Wonders"
+
+app.locals.info = results;
+
+app.get('/', (request, response) => {
+  response.send("Sports' Greatest One Hit Wonders!")
+})
+
+app.get('/api/v1/sports', (request, response) => {
+  const { info } = app.locals
+
+  response.json({ info })
+})
+
+app.get('/api/v1/sports/:id', (request, response) => {
+  const { id } = request.params
+
+  const sport = app.locals.info.find(item => item.id === id)
+})
