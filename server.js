135224d7f6b6b258fcfe528288adf50feaaf6cf6
Sara <saralkarsh@gmail.com>
2019-11-23 13:06:49 -0700

    Add app.listen and confirm GET for leagues works

--- server.js
+++ server.js
@@ -2,26 +2,40 @@
 // const csv = require('csv-parser')
 const express = require('express')
 const app = express()
-const results = []
-
 const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
 
 
 app.set('port', process.env.PORT || 3000)
 app.use(express.json())
 app.locals.title = "Sports' One Hit Wonders"
 
-app.locals.info = results;
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')} `)
+})
+
+
 
 app.get('/', (request, response) => {
   response.send("Sports' Greatest One Hit Wonders!")
 })
 
-app.get('/api/v1/sports', (request, response) => {
-  const { info } = app.locals
-
-  response.json({ info })
+app.get('/api/v1/leagues', (request, response) => {
+  database('leagues').select()
+    .then((leagues) => {
+      response.status(200).json(leagues)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
 })
+
+// app.get('/api/v1/leagues', (request, response) => {
+//   const { info } = app.locals
+
+//   response.json({ info })
+// })
 
 app.get('/api/v1/sports/:id', (request, response) => {
   const { id } = request.params
